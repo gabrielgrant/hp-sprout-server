a5ca76f811506e9fd3e5d8ba888f6effb56c78d4
Gabriel Grant <g@briel.ca>
2014-12-06 16:51:30 -0700

    initial import

---
+++ Nancy.DemoApplication1/Modules/IndexModule.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Nancy.DemoApplication1.Modules
+{
+
+
+    using HP.PC.Presentation;
+    using Nancy;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Windows.Media.Imaging;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            string product = "hello1";
+
+
+
+            Get["/"] = _ => View["index"];
+
+            Get["/capture"] = _ =>
+            {
+
+
+              
+
+
+                var response = (Response)getMoment();
+                response.ContentType = "application/json";
+                return response;
+            };
+
+        }
+
+
+        public class ImageResult
+        {
+            public IPcPicture metadata = null;
+            public String imageData = null;
+        }
+        public string getMoment() {
+            using (var sdk = HPPC.CreateLink())
+            {
+
+                var moment =  sdk.CaptureMomentAsync().Result;
+
+
+                // Extract the top-level picture and child images.
+                var picture = sdk.ExtractPictureAsync(moment).Result;
+
+                // Extract the top-level outline and child outlines.
+                var outline = sdk.ExtractOutlineAsync(moment).Result;
+               // DynamicDictionary d = new DynamicDictionary();
+
+                List<ImageResult> images = new List<ImageResult>();
+                foreach (var pic in picture.Children)
+                {
+                    ImageResult imres= new ImageResult();
+                    imres.metadata=pic;
+                    MemoryStream ms = new MemoryStream();
+                    
+                    PngBitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(pic.Image));
+                    encoder.Save(ms);
+                    byte[] bitmapdata = ms.ToArray();
+                    
+
+                   // Convert.ToBase64String();
+                    imres.imageData = "data:image/png;base64,"+Convert.ToBase64String(bitmapdata); 
+                    images.Add(imres);
+
+                }
+
+                string json = JsonConvert.SerializeObject(images);
+                return json;
+            }
+        }
+
+
+    }
+
+
+    
+}
